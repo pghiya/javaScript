8c08c2b41bf91d2a85b0498ca9991a5916c712ab
Pranay Ghiya <pghiya@tibco.com>
2019-06-21 22:02:11 +0530

    FIND OUT MAXIMUM NUM. FROM A ARRAY OF SIZE N

--- CodingQuestions/FindOutMinFromArray.js
+++ CodingQuestions/FindOutMinFromArray.js
@@ -1,8 +1,8 @@
 
-// Q. FIND OUT MAXIMUM NUM. FROM A ARRAY OF SIZE N WITHOUT USING INBUILT METHODS
+// Q. FIND OUT MINIMUM NUM. FROM A ARRAY OF SIZE N WITHOUT USING INBUILT METHODS
 
 // There are 2 ways in wich I prefer to solve this
-// 1st - This can be also used if asked to sort array in descending
+// 1st - This can be also used if asked to sort array in ascending
 var a, size, temp;
 
 a = [1, 10, 5, 12, 4, 13, 19]; //Give any random inputs
@@ -11,7 +11,7 @@ temp; //Temp variable used for swapping
 
 for (var i = 0; i < 7; i++) {  //Outer loop to hold the elemnt value in each iteration
   for (var j = 0; j < 7; j++) {  //Inner loop to iterate over array and consider element for comparison
-    if (a[i] > a[j] && i != j) { //Condition for checking Max value
+    if (a[i] < a[j] && i != j) { //Condition for checking Min value
       temp = a[i];
       a[i] = a[j];
       a[j] = temp;
@@ -24,21 +24,21 @@ console.log(a[0]);
 //--------------------------------------------------------------------------------------------------
 
 // 2nd
-var a, max;
+var a, Min;
 
 a = [1, 10, 50, 12, 41, 13, 19];  //Give any random inputs
-Max = a[0];  //Consider 1st element as Max
+Min = a[0];  //Consider 1st element as Min
 
 for (var i = 1; i < 7; i++) { //Iterate over array starting 2nd element.
-  if (a[i] > Max) { //Check if element at current index is greater than Max
-    Max = a[i];  //If yes, now assign Max to new value i.e. element at that index 
+  if (a[i] < Min) { //Check if element at current index is less than Min
+    Min = a[i];  //If yes, now assign Min to new value i.e. element at that index 
   }
 }
 
-console.log(Max);
+console.log(Min);
 
 //-------------------------------------------------------------------------------------------------
 
 //3rd If allowed to used Sort() method
 
-a.sort(function(a,b){return a-b;}) //Sorting works alphabetically, So need to add function to sort numerically
+a.sort(function(a,b){return b-a;}) //Sorting works alphabetically, So need to add function to sort numerically
