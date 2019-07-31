09d7fc5f6447cac5262a12c23256ab5b886239b9
Pranay Ghiya <pghiya@tibco.com>
2019-07-31 17:28:32 +0530

    Following changes made under this commit :
    1. Included bootstrap to be used in application.
    2. Created menu with 2 options - Home & Github User Details.
    3. Implemented routing for to & fro between menu items
    4. Created 2 new components based on menu items

--- gitHubUserDetails/src/app/app-routing.module.ts
+++ gitHubUserDetails/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from '../app/home/home.component'; //Imported Home Component
+import {ApplicationViewComponent} from '../app/application-view/application-view.component' //Imported Application View Component 
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent}, // added route path for homepage
+  {path:'appView', component:ApplicationViewComponent}, //added route path for application view page
+  {path: '**', redirectTo: 'home'} //Implemented to make sure whnever application is loaded it lands on the home page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
